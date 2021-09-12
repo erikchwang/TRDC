01fe73c66cee8e57c0047d2f26f6cf867822dd67
Chao Wang <erik.chwang@gmail.com>
2021-09-12 10:03:09 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TRDC
+
+## setup
+
+Run "sh setup.sh" in a "Linux + GPU" environment.
+
+## launch
+
+Run "sh launch.sh" to perform a "preprocessing-training-evaluation" pipeline.
--- launch.sh
+++ launch.sh
@@ -2,11 +2,17 @@ ROOT=$(dirname $(realpath ${0}))
 SCALE=$(nvidia-smi --list-gpus | wc -l)
 source ${ROOT}/anaconda/bin/activate ${ROOT}/anaconda
 
+# preprocess the raw data to obtain the statistics
 python ${ROOT}/preprocess.py 0
+
+# preprocess the raw data to generate and split the dataset
 python ${ROOT}/preprocess.py 1
 
+# construct the model
 python ${ROOT}/construct.py
 
+# optimize the model on multiple GPUs in parallel
 python -m torch.distributed.launch --use_env --nproc_per_node=${SCALE} ${ROOT}/optimize.py
 
+# execute the trained model on the test set
 python ${ROOT}/execute.py
